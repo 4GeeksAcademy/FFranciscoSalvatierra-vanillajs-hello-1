225a18e96cf5692ba5c69a25c50758c8bf769387
FranSalvatierra16 <103911827+FranSalvatierra16@users.noreply.github.com>
2023-07-11 02:06:44 +0000

    Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#the-excuse").innerHTML = generandoLink();
+};
+
+let generandoLink = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let aux = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        aux.push(pronoun[i] + adj[b] + noun[c] + ".com ");
+        aux.push("<br />");
+      }
+    }
+  }
+  console.log(aux);
+  return aux;
 };
